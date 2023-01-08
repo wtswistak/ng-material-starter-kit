b8d3becaf75aa529b675afffe6c8ec88aa1180e9
wojtek <wswistak@student.agh.edu.pl>
2023-01-08 23:35:45 +0100

    4 List-Loading selections

--- src/app/components/category-list/category-list.component-module.ts
+++ src/app/components/category-list/category-list.component-module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
 import { CategoryListComponent } from './category-list.component';
 
 @NgModule({
-  imports: [MatCheckboxModule, CommonModule, MatProgressSpinnerModule],
+  imports: [MatCheckboxModule, CommonModule, MatProgressSpinnerModule, MatListModule],
   declarations: [CategoryListComponent],
   providers: [],
   exports: [CategoryListComponent]
