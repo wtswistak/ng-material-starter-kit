648b5ff700b6983f800a0f67e3c145a95542dcb3
wojtek <wswistak@student.agh.edu.pl>
2023-01-09 00:11:18 +0100

    2 List-Multi-Table-products

--- src/app/components/product/product.component-module.ts
+++ src/app/components/product/product.component-module.ts
@@ -5,10 +5,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTableModule } from '@angular/material/table';
 import { ProductComponent } from './product.component';
 
 @NgModule({
-  imports: [MatCheckboxModule, CommonModule, MatListModule, MatButtonModule, MatMenuModule, MatProgressSpinnerModule],
+  imports: [MatCheckboxModule, CommonModule, MatListModule, MatButtonModule, MatMenuModule, MatProgressSpinnerModule, MatTableModule],
   declarations: [ProductComponent],
   providers: [],
   exports: [ProductComponent]
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -2,4 +2,5 @@ export interface ProductModel {
   readonly title: string;
   readonly price:string;
   readonly image:string;
+  readonly category:string;
 }
